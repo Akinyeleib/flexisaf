bcdb714c98d343e23674e47a49b49451c179bf40
Akinyeleib <iakinyele3@gmail.com>
2025-09-14 20:34:02 +0100

    Modify Home Route

--- src/main/java/com/flexisaf/tasks/TasksApplication.java
+++ src/main/java/com/flexisaf/tasks/TasksApplication.java
@@ -2,12 +2,20 @@ package com.flexisaf.tasks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class TasksApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TasksApplication.class, args);
+	}
+
+	@GetMapping()
+	public String welcome() {
+		return "Welcome to Flexisaf";
 	}
 
 }
