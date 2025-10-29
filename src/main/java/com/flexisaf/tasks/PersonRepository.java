deadda316d53f2f10e54b075606a388388b32a4e
Akinyeleib <iakinyele3@gmail.com>
2025-10-29 20:56:44 +0100

    move Person class to /model

--- src/main/java/com/flexisaf/tasks/PersonRepository.java
+++ src/main/java/com/flexisaf/tasks/PersonRepository.java
@@ -1,5 +1,6 @@
 package com.flexisaf.tasks;
 
+import com.flexisaf.tasks.model.Person;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
