62dd0783a9e3684cb3cc98f72d1e18532b3dd3bd
Akinyeleib <iakinyele3@gmail.com>
2025-10-29 21:28:39 +0100

    Handle errors effectively

---
+++ src/main/java/com/flexisaf/tasks/exception/ErrorHandler.java
@@ -0,0 +1,38 @@
+package com.flexisaf.tasks.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class ErrorHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(FailedValidationException.class)
+    public Map<String, String> handleFailedValidationException(FailedValidationException f) {
+        Map<String, String> data = new HashMap<>();
+        data.put("message", f.getMessage());
+        return data;
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(PersonNotFoundException.class)
+    public Map<String, String> handlePersonNotFoundException(PersonNotFoundException p) {
+        Map<String, String> data = new HashMap<>();
+        data.put("message", p.getMessage());
+        return data;
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(Exception.class)
+    public Map<String, String> handleGenericException(Exception p) {
+        Map<String, String> data = new HashMap<>();
+        data.put("message", p.getMessage());
+        return data;
+    }
+
+}
